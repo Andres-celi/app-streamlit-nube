536870251ea8b8ff12596bf201adefcc13b54ef3
Andres-celi <oswaldo.celivega@uqconnect.edu.au>
2025-05-22 20:54:34 -0500

    test

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-# URL = "http://localhost:8080/predict/"
-URL = "https://model-flask-docker-olal.onrender.com/predict/"
+URL = "https://reroes-ml01-latest.onrender.com/predict/"
+#URL = "https://model-flask-docker-olal.onrender.com/predict/"
